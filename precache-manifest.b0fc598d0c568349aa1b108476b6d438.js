05def81f9ab65a3ca26b73d3347ad357fee1e0c4
Derrick <dderrickmatheww@gmail.com>
2019-07-03 18:05:38 -0400

    Updates

---
+++ precache-manifest.b0fc598d0c568349aa1b108476b6d438.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b35837d16e6e036e464f0ca96a34592",
+    "url": "/ReactMemoryGame/index.html"
+  },
+  {
+    "revision": "543a5e6b598cebcc08f7",
+    "url": "/ReactMemoryGame/static/css/main.ae365aa3.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ReactMemoryGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "543a5e6b598cebcc08f7",
+    "url": "/ReactMemoryGame/static/js/main.7c95a987.chunk.js"
+  },
+  {
+    "revision": "892c58de1f85aeb180fb",
+    "url": "/ReactMemoryGame/static/js/runtime~main.5b9475f6.js"
+  }
+]);
